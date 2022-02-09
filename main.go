6615625e2f02d2454d97b0ccdaee4b223791f81c
Takumi Kanada <candy12t@gmail.com>
2022-02-09 21:15:26 +0900

    Input file with option

--- main.go
+++ main.go
@@ -1,23 +1,69 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/candy12t/sudoku/borad"
 )
 
 func main() {
-	b := borad.Borad{
-		{0, 1, 2, 3, 0, 0, 0, 0, 0},
-		{0, 4, 5, 6, 0, 0, 0, 8, 0},
-		{0, 7, 8, 9, 0, 0, 0, 0, 0},
-		{7, 0, 0, 0, 2, 0, 0, 0, 5},
-		{0, 0, 0, 4, 5, 6, 0, 0, 0},
-		{5, 0, 0, 0, 8, 0, 0, 0, 6},
-		{0, 0, 0, 0, 0, 1, 2, 3, 0},
-		{0, 3, 0, 0, 0, 4, 5, 6, 0},
-		{0, 0, 0, 0, 0, 7, 8, 9, 0},
+	if len(os.Args) == 1 {
+		fmt.Println("Error: require sudoku file path")
+		os.Exit(1)
 	}
+
+	filePath := os.Args[1]
+	data, err := readFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := initBorad(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	b.Backtrack()
-	fmt.Println(b)
+	fmt.Print(b)
+}
+
+func initBorad(line string) (borad.Borad, error) {
+	var b borad.Borad
+	br := bytes.NewBufferString(line)
+	s := bufio.NewScanner(br)
+	s.Split(bufio.ScanRunes)
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if !s.Scan() {
+				break
+			}
+			token := s.Text()
+			if token == "." {
+				b[i][j] = 0
+				continue
+			}
+			n, err := strconv.Atoi(token)
+			if err != nil {
+				return borad.Borad{}, err
+			}
+			b[i][j] = n
+		}
+	}
+	return b, nil
+}
+
+func readFile(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	line := strings.Replace(string(data), "\n", "", 9)
+	return line, nil
 }
